db964c256e0cd2e24b9618fae65059e634597506
jkl221087 <jkl70110@gmail.com>
2026-01-30 03:43:20 +0800

    dp class

--- DynamicProgramming/cansum.py
+++ DynamicProgramming/cansum.py
@@ -2,13 +2,40 @@ def canSum(targetnum, numbers):
     if targetnum == 0:
         return True
     for num in numbers:
-        reminder = targetnum -  num
-        if canSum(reminder, numbers) == True:
+        remainder = targetnum - num
+        if canSum(remainder, numbers) == True:
             return True
     return False
 
 
 
+def memoziation_canSum(targetnum, numbers, memo = {}):
+    if targetnum in memo:return memo[targetnum]
+    if targetnum == 0:return True
+    if targetnum < 0:return False
+
+    for num in numbers:
+        remainder = targetnum - num
+        if canSum(remainder, numbers, memo) == True:
+            memo[targetnum] = True
+            return True
+    memo[targetnum] == False
+    return False
+
+
+c = memoziation_canSum(7, [7, 14])
+print(c)
+
+
+#casum 暴力解
+#m = target num
+#m = array length
+#最壞情況到最低節點高度m   -1 m 次
+#假設長度3的array 每一個節點都會有三個子節點 n = 3 高度m次 O(n^m) 指數成長
+
+
+
+#memoation優化
 #子樹布林傳給二元樹如果其中一個是True parent return True
-#
-#
+#O(m * n)time
+#O(m)space
